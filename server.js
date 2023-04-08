23995fa8d768434348f0e83b9db57001e77c9f7c
Vahe <8384838z@gmail.com>
2023-04-08 21:23:17 +0300

    Added POST request to API

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const axios = require("axios");
 
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
@@ -18,7 +19,6 @@ app.get("/", (req, res) => {
 app.get("/thanks.html", (req, res) => {
   res.sendFile(__dirname + "/thanks.html");
 });
-
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
@@ -61,6 +61,47 @@ app.post("/", function (req, res) {
     chatId,
     `НОВЫЙ ЛИД!\n\nИмя: ${newLead.firstName}\nФамилия: ${newLead.lastName}\nE-mail: ${newLead.email}\nНомер телефона: ${newLead.telephone}`
   );
-
   res.redirect("/thanks.html");
+
+  const flowHash = "63fe0bac3516006064";
+  const clientIp = req.ip;
+  const landingName = "Лиды";
+  // данные для отправки в POST запросе
+  const postData = {
+    flow_hash: flowHash,
+    landing: "eagehwnrg",
+    first_name: newLead.firstName,
+    last_name: newLead.lastName,
+    email: newLead.email,
+    phone: newLead.telephone,
+    ip: "93.223.752.15",
+    sub1: "tetfsegfebwub_1",
+    sub2: "tetfsbwfub_2",
+    sub3: "teteeegegfsubwb_3",
+    sub4: "tetetgeubfwb_4",
+    click_id: "clbwgwyefgeick_id",
+    user_agent:
+      "iPhone OSy 14_3 flik Mac OS X) AppleegegWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.2 Mobile/15E148 Safari/604.1",
+  };
+  // конфигурация запроса
+  const requestConfig = {
+    method: "post",
+    url: "https://cryp.im/api/v1/web/conversion?api_token=RGLVDNfSZe5ccfEPsE715g2VmTDqAFyrRhbZ83C8b0pQb8XiTTUudrJtqkLn",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: postData,
+  };
+
+  let APIResponse;
+  // отправка запроса и обработка ответа
+  axios(requestConfig)
+    .then((response) => {
+      APIResponse = response.data;
+      bot.sendMessage(chatId, `Ответ от API\n\n${JSON.stringify(APIResponse)}`);
+    })
+    .catch((error) => {
+      APIResponse = error;
+      bot.sendMessage(chatId, `Ответ от API\n\n${JSON.stringify(APIResponse)}`);
+    });
 });
